49270764f24f29a1b1b1f27907666b202b2d2035
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-10-29 19:59:25 -0500

    Finish navbar

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,25 @@
-const btnHamburger = document.querySelector('#btnHamburger');
-const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay');
+const body = document.querySelector("body");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElements = document.querySelectorAll(".has-fade");
 
-btnHamburger.addEventListener('click', function(){
-    if (header.classList.contains('open')) {
-        // Close hamburger menu
-        header.classList.remove('open');
-        overlay.classList.add('fade-out');
-        overlay.classList.remove('fade-in');
-    }
-    else {
-        // Open hamburger menu
-        header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
-    }
+btnHamburger.addEventListener("click", function () {
+  if (header.classList.contains("open")) {
+    // Close hamburger menu
+    body.classList.remove("no-scroll");
+    header.classList.remove("open");
+    fadeElements.forEach(function (element) {
+      element.classList.add("fade-out");
+      element.classList.remove("fade-in");
+    });
+  } else {
+    // Open hamburger menu
+    body.classList.add("no-scroll");
+    header.classList.add("open");
+    fadeElements.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
 });
